87566c4262225d83fda1be39daf2125597e7daee
Rafał Olszewicz <rafalolszewicz@gmail.com>
2019-12-21 19:08:25 +0100

    upd feathericons cdnjs.min.js

--- templates/parts/scripts/_feathericons.php
+++ templates/parts/scripts/_feathericons.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- Feather Icons  https://feathericons.com/ -->
-<script defer src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.24.1/feather.js"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.24.1/feather.min.js"></script>
 <script>
 	window.addEventListener('load', function() {
 	  feather.replace()
